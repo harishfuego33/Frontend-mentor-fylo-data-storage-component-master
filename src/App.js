9d3ffe8043c3f467e0730f77aa624ec1679c65ea
harishfuego33 <harishkumar.ct20@bitsathy.ac.in>
2024-04-26 19:51:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./index.css";
+export default function App() {
+	return (
+		<main className="section-main">
+			<Options />
+			<StorageMeter />
+			<Level />
+		</main>
+	);
 }
-
-export default App;
+function Options() {
+	return (
+		<section className="section__1">
+			<div className="section__1-heading">
+				<img src="/images/logo.svg" alt="logo" className="section__1-logo" />
+			</div>
+			<div className="section__1-icons">
+				<div className="section__1-icon-box">
+					<img
+						src="/images/icon-document.svg"
+						alt="logo"
+						className="section__1-icon"
+					/>
+				</div>
+				<div className="section__1-icon-box">
+					<img
+						src="/images/icon-folder.svg"
+						alt="logo"
+						className="section__1-icon"
+					/>
+				</div>
+				<div className="section__1-icon-box">
+					<img
+						src="/images/icon-upload.svg"
+						alt="logo"
+						className="section__1-icon"
+					/>
+				</div>
+			</div>
+		</section>
+	);
+}
+function Level() {
+	return (
+		<section className="section__2">
+			<div className="section__2-heading">
+				You've used <strong> 815 GB</strong> of your storage
+			</div>
+			<div className="level__scale">
+				<div className="level"></div>
+			</div>
+			<div className="digital__scale">
+				<span className="instial__level">0 GB</span>
+				<span className="final__level">1000 GB</span>
+			</div>
+		</section>
+	);
+}
+function StorageMeter() {
+	return (
+		<div className="storage__meter">
+			<strong>185</strong> <span>GB LEFT</span>
+		</div>
+	);
+}
